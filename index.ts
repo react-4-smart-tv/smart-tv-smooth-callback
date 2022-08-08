9433af50bcc9d3c8f61c2bf82d3524e2c8364dc9
TcoND <congnst@gviet.vn>
2022-08-08 14:45:18 +0700

    update

--- index.ts
+++ index.ts
@@ -1,76 +1,124 @@
+const nowCustom = () => {
+  try {
+    return window.performance.now();
+  } catch (error) {
+    return Date.now();
+  }
+};
+
+const _now =
+  nowCustom ||
+  function () {
+    return new Date().getTime();
+  };
+
 export type Cancellable<T extends (...args: any[]) => void> = {
-    (...args: Parameters<T>): void;
-    cancel(): void;
+  (...args: Parameters<T>): void;
+  cancel(): void;
 };
 
 type WindowWithoutRAF = Omit<Window, "requestAnimationFrame">;
 
 type WrapperState = {
-    cancelToken: number;
-    callbackThis?: any;
-    args?: any[];
+  cancelToken: number;
+  callbackThis?: any;
+  args?: any[];
 };
 
 const wrapperFactory = function () {
-    const state: WrapperState = {
-        cancelToken: 0,
-    };
+  const state: WrapperState = {
+    cancelToken: 0,
+  };
 
-    const resetCancelToken = () => {
-        state.cancelToken = 0;
-    };
+  const resetCancelToken = () => {
+    state.cancelToken = 0;
+  };
 
-    const wrapper = <T extends Function>(cbThis: any, cb: T, ...args: any[]) => {
-        state.callbackThis = cbThis;
-        state.args = args;
+  const wrapper = <T extends Function>(cbThis: any, cb: T, ...args: any[]) => {
+    state.callbackThis = cbThis;
+    state.args = args;
 
-        if (state.cancelToken) {
-            return;
-        }
+    if (state.cancelToken) {
+      return;
+    }
 
-        if ("requestAnimationFrame" in window) {
-            state.cancelToken = window.requestAnimationFrame(() => {
-                cb.apply(state.callbackThis, state.args);
-                resetCancelToken();
-            });
-        } else {
-            cb.apply(state.callbackThis, state.args);
-            state.cancelToken = (window as WindowWithoutRAF).setTimeout(
-                resetCancelToken,
-                1000 / 60
-            );
-        }
-    };
-
-    wrapper.cancel = () => {
-        if ("requestAnimationFrame" in window) {
-            window.cancelAnimationFrame(state.cancelToken);
-        }
-        window.clearTimeout(state.cancelToken);
+    if ("requestAnimationFrame" in window) {
+      state.cancelToken = window.requestAnimationFrame(() => {
+        cb.apply(state.callbackThis, state.args);
         resetCancelToken();
-    };
+      });
+    } else {
+      cb.apply(state.callbackThis, state.args);
+      state.cancelToken = (window as WindowWithoutRAF).setTimeout(
+        resetCancelToken,
+        1000 / 60
+      );
+    }
+  };
 
-    return wrapper as Cancellable<typeof wrapper>;
+  wrapper.cancel = () => {
+    if ("requestAnimationFrame" in window) {
+      window.cancelAnimationFrame(state.cancelToken);
+    }
+    window.clearTimeout(state.cancelToken);
+    resetCancelToken();
+  };
+
+  return wrapper as Cancellable<typeof wrapper>;
 };
 
 const throttleFactory = function <T extends (...args: any[]) => void>(
-    callback: T,
-    thisArg?: any,
-    ...argArray: any[]
+  callback: T,
+  thisArg?: any,
+  ...argArray: any[]
 ): Cancellable<T> {
-    const wrapper = wrapperFactory();
-    const argCount = arguments.length;
-    const throttledCallback = function (this: any, ...args: Parameters<T>) {
-        wrapper(argCount > 1 ? thisArg : this, callback, ...argArray, ...args);
-    };
-    throttledCallback.cancel = () => wrapper.cancel();
-    return throttledCallback;
+  const wrapper = wrapperFactory();
+  const argCount = arguments.length;
+  const throttledCallback = function (this: any, ...args: Parameters<T>) {
+    wrapper(argCount > 1 ? thisArg : this, callback, ...argArray, ...args);
+  };
+  throttledCallback.cancel = () => wrapper.cancel();
+  return throttledCallback;
 };
 
 export const smoothFunction = <T extends (...args: any[]) => void>(
-    callback: T
+  callback: T
 ): Cancellable<T> => {
-    const throttledCallback = throttleFactory(callback);
-    throttledCallback.bind = throttleFactory.bind(null, callback);
-    return throttledCallback;
+  const throttledCallback = throttleFactory(callback);
+  throttledCallback.bind = throttleFactory.bind(null, callback);
+  return throttledCallback;
+};
+
+export const setRequestTimeout = (fn: Function, delay = 0, ...args: any[]) => {
+  let this_ = this;
+  const start = _now();
+  const handle: { [key in string]: any } = {};
+  const loop = () => {
+    const current = _now();
+    const delta = current - start;
+    if (delta >= delay) {
+      fn.call(this_, ...args);
+    } else {
+      if ("requestAnimationFrame" in window) {
+        handle.value = window.requestAnimationFrame(loop);
+      } else {
+        handle.value = (window as WindowWithoutRAF).setTimeout(loop, 1000 / 60);
+      }
+    }
+  };
+  if ("requestAnimationFrame" in window) {
+    handle.value = window.requestAnimationFrame(loop);
+  } else {
+    handle.value = (window as WindowWithoutRAF).setTimeout(loop, 1000 / 60);
+  }
+  return handle;
+};
+
+export const clearRequestTimeout = (handle: any) => {
+  if (typeof handle === "undefined" || handle === null) return;
+  if ("requestAnimationFrame" in window) {
+    window.cancelAnimationFrame(handle.value);
+    return;
+  }
+  window.clearTimeout(handle.value);
 };
--- lib/esm/index.d.ts
+++ lib/esm/index.d.ts
@@ -3,3 +3,7 @@ export declare type Cancellable<T extends (...args: any[]) => void> = {
     cancel(): void;
 };
 export declare const smoothFunction: <T extends (...args: any[]) => void>(callback: T) => Cancellable<T>;
+export declare const setRequestTimeout: (fn: Function, delay?: number, ...args: any[]) => {
+    [x: string]: any;
+};
+export declare const clearRequestTimeout: (handle: any) => void;
